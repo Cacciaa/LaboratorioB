2353ab48ccff1d163b315ad24089fc26dacd20f3
Loryerba <72017837+Loryerba@users.noreply.github.com>
2023-05-17 20:26:45 +0200

    Server project base

---
+++ Server/src/server/Canzoni.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package server;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class Canzoni implements Serializable {
+    
+    private String titolo;
+    
+    private String autore;
+    
+    private int anno;
+
+    public Canzoni(String titolo, String autore, int anno) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.anno = anno;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+    
+    
+    
+    
+}
