3f1a6efea493fcba210a294cec234e2da5b88ff3
unknown <lorrenzoerba250@gmail.com>
2023-05-19 11:38:34 +0200

    EmotionalSongs Project with Maven

---
+++ EmotionalSongs/src/main/java/emotionalsongs/UtentiRegistrati.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package emotionalsongs;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class UtentiRegistrati implements Serializable {
+
+    private String codiceFiscale;
+    
+    private String nome;
+    
+    private String cognome;
+    
+    private String citta;
+    
+    private int cap;
+    
+    private String via;
+    
+    private int civico;
+    
+    private String email;
+    
+    private String password;
+
+    public UtentiRegistrati(String codiceFiscale, String nome, String cognome, String citta, int cap, String via, int civico, String email, String password) {
+        this.codiceFiscale = codiceFiscale;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.citta = citta;
+        this.cap = cap;
+        this.via = via;
+        this.civico = civico;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public int getCivico() {
+        return civico;
+    }
+
+    public void setCivico(int civico) {
+        this.civico = civico;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+}
